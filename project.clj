0f115d93edd97769ff1e2d8c7f99488cc89431ad
Nils Blum-Oeste <mail@nils-blum-oeste.net>
2015-11-05 20:28:08 +0100

    Initial commit

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject compo-tests "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :main ^:skip-aot compo-tests.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
